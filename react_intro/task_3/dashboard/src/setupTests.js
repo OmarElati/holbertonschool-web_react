9188d23f75b338cc610c244b3f55b18c3f845935
OmarElati <5796@holbertonstudents.com>
2024-04-28 05:07:07 +0200

    Install Enzyme

--- react_intro/task_3/dashboard/src/setupTests.js
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -1,6 +1,4 @@
-// Import Enzyme and the adapter for React 16
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-// Configure Enzyme with React 16 adapter
 Enzyme.configure({ adapter: new Adapter() });
