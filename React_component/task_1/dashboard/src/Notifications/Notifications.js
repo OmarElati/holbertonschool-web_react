c4c86907412cafb2993173c51056efd6ea7ccce4
OmarElati <5796@holbertonstudents.com>
2024-05-12 19:05:19 +0200

    Lifecycles

---
+++ React_component/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import close_icon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape'
+
+const Notifications = ({displayDrawer, listNotifications}) => {
+  const btnStyle = {
+    float: 'right',
+    height: '25px',
+    width: '25px',
+    position: 'absolute',
+    top: '16px',
+    right: '16px',
+    background: 'none',
+    border: 'none',
+  }
+
+  return (
+    <div className="container">
+    <div className="menuItem">
+      <p>Your notifications</p>
+    </div>
+
+    { displayDrawer &&
+    <div className="Notifications">
+      <button aria-label="Close" style={btnStyle} onClick={() => console.log('Close button has been clicked')}>
+        <img src={close_icon} alt="close-icon"/>
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+      {listNotifications.map(notification => <NotificationItem 
+                      key={notification.id} 
+                      type={notification.type} 
+                      value={notification.value} 
+                      html={notification.__html}/>)}
+      </ul>
+    </div> }
+    </div>
+  );
+};
+
+Notifications.prototype = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.shape(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+};
+
+export default Notifications;
