5c029e4020cab3f408d4a7ce49129bae82949a6b
OmarElati <5796@holbertonstudents.com>
2024-05-12 20:24:24 +0200

    Update_v1

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import BodySection from './BodySection';
-import './BodySection.css'
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 const BodySectionWithMarginBottom = (props) => {
+    const styles = StyleSheet.create({
+        mgbot: {
+            marginBottom: 40
+        },
+        space: {
+            width: '100%'
+        }
+    })
     return (
-        <div className='bodySectionWithMargin'>
+        <div className={css(styles.mgbot, styles.space)}>
             <BodySection {...props}/>
         </div>
     )
