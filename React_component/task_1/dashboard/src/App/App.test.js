59e3cb3ed402c026cd5c4a28e51ff4f90d09cbee
OmarElati <5796@holbertonstudents.com>
2024-05-12 22:00:31 +0200

    commit

--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -3,66 +3,48 @@ import React from 'react';
 import App from './App';
 import Login from '../Login/Login';
 
-
-// shallow render app component
 describe('<App />', () => {
-	it('Tests that App renders without crashing', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.exists()).toBe(true);
-	})
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-	it('Contains Header component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find('Header').length).toBe(1);
-	})
+    it('Tests that App renders without crashing', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.exists()).toBe(true);
+    });
 
-	it('Contains Login component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.contains(<Login />)).toBe(true);
-	})
+    it('Contains Header component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Header').length).toBe(1);
+    });
 
-	it('Contains Footer component', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find('Footer').length).toBe(1);
-	})
+    it('Contains Login component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.contains(<Login />)).toBe(true);
+    });
 
-	it('Tests that CourseList is not displayed', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find('CourseList').length).toBe(0);
-	})
-});
+    it('Contains Footer component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Footer').length).toBe(1);
+    });
 
+    it('Tests that CourseList is not displayed', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('CourseList').length).toBe(0);
+    });
 
-// describe case when isLoggedIn is true
-describe('<App />', () => {
-	it('Tests that the Login component is not rendered', () => {
-		const wrapper = shallow(<App isLoggedIn={true} />);
-		expect(wrapper.contains(<Login />)).toBe(false);
-	})
+    it('Verifies that the logOut function is called when ctrl-h is pressed', () => {
+        const logOutMock = jest.fn();
+        const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
 
-	it('Tests that CourseList component is rendered', () => {
-		const wrapper = shallow(<App isLoggedIn />);
-		expect(wrapper.find('CourseList').length).toBe(1);
-	})
+        const wrapper = mount(<App isLoggedIn logOut={logOutMock} />);
+        const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
 
-	// next 2 tests are under review, spyOn() is not working
-	// and I keep getting this error:
-	// TypeError: wrapper.instance(...).keyDownHandler is not a function
+        window.dispatchEvent(event);
 
-	it(`Verifies that alert is called when ctrl-h is pressed`, () => {
-		const AlertSpy = jest.spyOn(window, 'alert');
-		const wrapper = mount(<App isLoggedIn />);
-		wrapper.instance().keyDownHandler({ keyCode: 72, ctrlKey: true });
-		expect(AlertSpy).toHaveBeenCalledWith('Logging you out');
-		wrapper.unmount();
-	})
+        expect(alertMock).toHaveBeenCalledWith('Logging you out');
+        expect(logOutMock).toHaveBeenCalled();
 
-	it(`Verifies that logOut function is called when ctrl-h is pressed`, () => {
-		const ConsoleSpy = jest.spyOn(global.console, 'log');
-		const wrapper = mount(<App isLoggedIn />);
-		wrapper.instance().keyDownHandler({ keyCode: 72, ctrlKey: true });
-		expect(ConsoleSpy).toHaveBeenCalledWith('logOut function console log for testing');
-		wrapper.unmount();
-	})
-
+        wrapper.unmount();
+    });
 });
