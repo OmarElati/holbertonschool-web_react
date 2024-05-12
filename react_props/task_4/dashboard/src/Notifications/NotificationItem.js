91e9fc803c3dbed093fed3eed439dc47914077ed
OmarElati <5796@holbertonstudents.com>
2024-05-12 18:55:05 +0200

    Props types & Default props & Shapes

---
+++ react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NotificationItem = ({ type, html, value }) => {
+  if (html)
+    return (
+      <li data-priority={type} dangerouslySetInnerHTML={{ __html: html }}></li>
+    );
+  return (
+    <li data-priority={type}>{value}</li>
+  );
+};
+
+NotificationItem.propTypes = {
+  html: PropTypes.shape({__html: PropTypes.string}),
+  type: PropTypes.string,
+  value: PropTypes.string,
+};
+
+NotificationItem.defaultProps = {
+  type: "default",
+};
+
+export default NotificationItem;
