6f7830a2435e08f67fff82b5329fb23773174b1f
OmarElati <5796@holbertonstudents.com>
2024-05-26 17:49:26 +0200

    Bound the actions

--- 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -11,21 +11,27 @@ export const login = (email, password) => {
     user: { email, password },
   };
 };
+export const boundLogin = (email, password) => dispatch(login(email, password));
 
 export const logout = () => {
   return {
     type: LOGOUT,
   };
 };
+export const boundLogout = () => dispatch(logout());
 
 export const displayNotificationDrawer = () => {
   return {
     type: DISPLAY_NOTIFICATION_DRAWER,
   };
 };
+export const boundDisplayNotificationDrawer = () =>
+  dispatch(displayNotificationDrawer());
 
 export const hideNotificationDrawer = () => {
   return {
     type: HIDE_NOTIFICATION_DRAWER,
   };
 };
+export const boundHideNotificationDrawer = () =>
+  dispatch(hideNotificationDrawer());
